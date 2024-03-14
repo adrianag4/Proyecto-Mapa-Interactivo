267462e323619583f28da3ca6cf3816be13d8654
adrianag4 <adriana.gutierrez235633@potros.itson.edu.mx>
2024-03-13 20:11:31 -0700

    presentacion

---
+++ src/main/java/com/mycompany/presentacion/Presentacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.presentacion;
+
+/**
+ *
+ * @author Adriana Gutierrez, Esmeralda Molina, Josue Gomez, Roberto Favela
+ */
+public class Presentacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
