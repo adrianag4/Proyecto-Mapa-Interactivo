dc3f3b093be6020fc5d5cf564de95cf2a649ab98
melle23 <molinaestradaesmeralda@gmail.com>
2024-03-13 22:13:43 -0700

    Pantalla inicioSesion

--- src/main/java/Vistas/InicioSesion.java
+++ src/main/java/Vistas/InicioSesion.java
@@ -27,21 +27,85 @@ public class InicioSesion extends javax.swing.JDialog {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        ButtonAjustes = new javax.swing.JButton();
+        ButtonIniciarSesion = new javax.swing.JButton();
+        ButtonVerHorario = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+
+        jLabel2.setText("jLabel2");
+
+        jLabel3.setText("jLabel3");
+
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        ButtonAjustes.setBackground(new java.awt.Color(60, 135, 196));
+        ButtonAjustes.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        ButtonAjustes.setText("Ajustes");
+        ButtonAjustes.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ButtonAjustesActionPerformed(evt);
+            }
+        });
+        jPanel1.add(ButtonAjustes, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 300, 160, 40));
+
+        ButtonIniciarSesion.setBackground(new java.awt.Color(60, 135, 196));
+        ButtonIniciarSesion.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        ButtonIniciarSesion.setText("Iniciar Sesión");
+        ButtonIniciarSesion.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ButtonIniciarSesionActionPerformed(evt);
+            }
+        });
+        jPanel1.add(ButtonIniciarSesion, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 180, 160, 40));
+
+        ButtonVerHorario.setBackground(new java.awt.Color(60, 135, 196));
+        ButtonVerHorario.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        ButtonVerHorario.setText("Ver Horario");
+        ButtonVerHorario.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ButtonVerHorarioActionPerformed(evt);
+            }
+        });
+        jPanel1.add(ButtonVerHorario, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 240, 160, 40));
+
+        jLabel4.setIcon(new javax.swing.ImageIcon("C:\\Users\\molin\\OneDrive\\Documentos\\Mapa Interactivo\\Proyecto-Mapa-Interactivo\\src\\main\\java\\Utilerias\\Logo_ITSON (1).png")); // NOI18N
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 0, 260, 100));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\molin\\OneDrive\\Documentos\\Mapa Interactivo\\Proyecto-Mapa-Interactivo\\src\\main\\java\\Utilerias\\logoPotros.png")); // NOI18N
+        jLabel1.setMaximumSize(new java.awt.Dimension(500, 300));
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, 518));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 500, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void ButtonAjustesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ButtonAjustesActionPerformed
+       
+    }//GEN-LAST:event_ButtonAjustesActionPerformed
+
+    private void ButtonIniciarSesionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ButtonIniciarSesionActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_ButtonIniciarSesionActionPerformed
+
+    private void ButtonVerHorarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ButtonVerHorarioActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_ButtonVerHorarioActionPerformed
 
     /**
      * @param args the command line arguments
@@ -86,5 +150,13 @@ public class InicioSesion extends javax.swing.JDialog {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton ButtonAjustes;
+    private javax.swing.JButton ButtonIniciarSesion;
+    private javax.swing.JButton ButtonVerHorario;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
