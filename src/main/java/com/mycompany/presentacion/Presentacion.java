40e9ef335d059b380a160203c2a0dd37ae7afb15
adrianag4 <adriana.gutierrez235633@potros.itson.edu.mx>
2024-04-09 15:52:20 -0700

    cambios

--- src/main/java/com/mycompany/presentacion/Presentacion.java
+++ src/main/java/com/mycompany/presentacion/Presentacion.java
@@ -1,5 +1,7 @@
 package com.mycompany.presentacion;
 
+import Vistas.frmPrincipal;
+
 /**
  *
  * @author Adriana Gutierrez, Esmeralda Molina, Josue Gomez, Roberto Favela
@@ -7,6 +9,9 @@ package com.mycompany.presentacion;
 public class Presentacion {
 
     public static void main(String[] args) {
-        System.out.println("UN PUSHEO");
+
+        frmPrincipal p = new frmPrincipal();
+        p.setVisible(true);
+        
     }
 }
